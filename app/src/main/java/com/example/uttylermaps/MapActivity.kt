eeab9d98efdee4ab2e14885f8774b6d45ecf3951
dansauc <d.sau310@gmail.com>
2026-02-19 23:11:45 -0600

    added labels

--- app/src/main/java/com/example/uttylermaps/MapActivity.kt
+++ app/src/main/java/com/example/uttylermaps/MapActivity.kt
@@ -6,11 +6,20 @@ import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ProgressBar
+import com.mappedin.models.Space
 import androidx.appcompat.app.AppCompatActivity
 import com.mappedin.MapView
+import com.mappedin.models.AddLabelOptions
+import com.mappedin.models.Doors
+import com.mappedin.models.DoorsUpdateState
+import com.mappedin.models.GeometryUpdateState
+import com.mappedin.models.UpdateState
 import com.mappedin.models.GetMapDataWithCredentialsOptions
+import com.mappedin.models.LabelAppearance
+import com.mappedin.models.MapDataType
 import com.mappedin.models.Show3DMapOptions
 
+//from https://developer.mappedin.com/android-sdk
 class MapActivity : AppCompatActivity() {
     private lateinit var mapView: MapView
     private lateinit var loadingIndicator: ProgressBar
@@ -56,33 +65,95 @@ class MapActivity : AppCompatActivity() {
         mapView.getMapData(options) { result ->
             result
                 .onSuccess {
-                    Log.d("MappedinDemo", "getMapData success")
+                    Log.d("Mappedin", "getMapData success")
                     // Display the map.
                     mapView.show3dMap(Show3DMapOptions()) { r ->
                         r.onSuccess {
                             runOnUiThread {
+                                //Map is laoded and ready
                                 loadingIndicator.visibility = android.view.View.GONE
                             }
                             onMapReady(mapView)
                         }
                         r.onFailure {
+                            //error showing map
                             runOnUiThread {
                                 loadingIndicator.visibility = android.view.View.GONE
                             }
-                            Log.e("MappedinDemo", "show3dMap error: $it")
+                            Log.e("Mappedin", "show3dMap error: $it")
                         }
                     }
-                }.onFailure {
+                }.onFailure {// error loading map
                     runOnUiThread {
                         loadingIndicator.visibility = android.view.View.GONE
                     }
-                    Log.e("MappedinDemo", "getMapData error: $it")
+                    Log.e("Mappedin", "getMapData error: $it")
                 }
         }
     }
 
+
+
+
     // Place your code to be called when the map is ready here.
     private fun onMapReady(mapView: MapView) {
-        Log.d("MappedinDemo", "show3dMap success - Map displayed")
+
+        //make doors visible
+        mapView.updateState(
+            Doors.INTERIOR,
+            DoorsUpdateState(
+                visible = true,
+                color = "blue",
+                topColor = "navy",
+                opacity = 0.5
+            ),
+
+        )
+
+       //add labels to areas
+            mapView.mapData.getByType<Space>(MapDataType.SPACE) { result ->
+                result.onSuccess { spaces ->
+                    spaces.forEach { space ->
+                        mapView.updateState(
+                            space,
+                            GeometryUpdateState(
+                                interactive = true
+                            )
+                        ) { result ->
+                            result.onFailure {
+                                Log.e("Mappedin", "Failed to update space", it)
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        //add labels
+        mapView.mapData.getByType<Space>(MapDataType.SPACE) { result ->
+            result.onSuccess { spaces ->
+                for (space in spaces) {
+                    if (space.name.isNotEmpty()) {
+                        val color = "blue"
+                        val appearance =
+                            LabelAppearance(
+                                color = color,
+
+                            )
+                        mapView.labels.add(
+                            target = space,
+                            text = space.name,
+                            options = AddLabelOptions(
+                                labelAppearance = appearance,
+                                interactive = true
+                            ),
+                        )
+                    }
+                    if(space.type.equals("bathroom"))
+                }
+            }
+        }
+
     }
+
 }
